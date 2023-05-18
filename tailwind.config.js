6f48ddb4016708857de49139fd1cae2049c0648e
RalatPC <ralatrado@gmail.com>
2023-05-18 13:42:47 +0100

    added style-guide colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        DarkBlue: 'hsl(233, 26%, 24%)',
+        LimeGreen: 'hsl(136, 65%, 51%)',
+        BrightCyan: 'hsl(192, 70%, 51%)',
+        GrayishBlue: 'hsl(233, 8%, 62%)',
+        LightGrayishBlue: 'hsl(220, 16%, 96%)',
+        VeryLightGray: 'hsl(0, 0%, 98%)',
+        White: 'hsl(0, 0%, 100%)'
+      },
+    },
   },
   plugins: [],
 }
