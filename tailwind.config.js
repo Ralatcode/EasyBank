e9ac3eec91ce59d4046290e68bfbcd2d20664017
RalatPC <ralatrado@gmail.com>
2023-05-18 13:45:21 +0100

    updated device screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors : {
         DarkBlue: 'hsl(233, 26%, 24%)',
