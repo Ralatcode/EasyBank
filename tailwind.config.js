82295aa5ee186e61341d33ed30d11adfa9d9496d
RalatPC <ralatrado@gmail.com>
2023-05-31 19:05:43 +0100

    added a custom utility for unset all

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         LightGrayishBlue: 'hsl(220, 16%, 96%)',
         VeryLightGray: 'hsl(0, 0%, 98%)',
         White: 'hsl(0, 0%, 100%)'
-      },
+      }
     },
   },
   plugins: [],
