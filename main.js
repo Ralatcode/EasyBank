c8f09500a248d81a658398b1693f1a2236fbe9dc
RalatPC <ralatrado@gmail.com>
2023-06-03 21:20:15 +0100

    moved mobile nav and ul as custom class

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const menu_div = document.querySelector('.menu-div');
 menu_div.addEventListener('click', () =>{
     menu_div.classList.toggle('open');
 });
+
+// window.addEventListener('resize', ()=> {
+//     if (screen.width >= 768) {
+
+//     }
+// });
