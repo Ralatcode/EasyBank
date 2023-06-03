cd11449b45e41e9a0cf27874292fbc179b433c74
RalatPC <ralatrado@gmail.com>
2023-06-03 21:45:41 +0100

    remove mobile menu styling on 768px screen

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const menu_div = document.querySelector('.menu-div');
+const menuNav = document.querySelector('nav');
+const menu_ul = document.querySelector('ul');
 
 menu_div.addEventListener('click', () =>{
     menu_div.classList.toggle('open');
 });
 
-// window.addEventListener('resize', ()=> {
-//     if (screen.width >= 768) {
-
-//     }
-// });
+window.addEventListener('resize', ()=> {
+    if (window.innerWidth >= 768) {
+        menuNav.classList.remove('mobile-nav');
+        menu_ul.classList.remove('mobile-ul');
+    } else {
+        menuNav.classList.add('mobile-nav');
+        menu_ul.classList.add('mobile-ul');
+    }
+});
